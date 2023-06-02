c12d5de7a310fe492ce0a173be0db85448f0cc8c
geek-nath <nath.codes@gmail.com>
2023-06-03 00:55:37 +0100

    suttup appwrite for projects

---
+++ src/appwriteConfig.js
@@ -0,0 +1,7 @@
+import { Client, Account } from 'appwrite';
+
+const client = new Client()
+    .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
+    .setProject('647a6e73beaa05950edd'); // Your project ID
+
+export const registerAccount = new Account(client);
