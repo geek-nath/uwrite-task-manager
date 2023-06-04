d875a24dc0890edbbdad6522e54f790df68b75b8
geek-nath <nath.codes@gmail.com>
2023-06-04 21:59:51 +0100

    cleared blanl spaces

--- src/appwriteConfig.js
+++ src/appwriteConfig.js
@@ -1,7 +1,5 @@
 import { Client, Account } from 'appwrite';
-
 const client = new Client()
     .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
     .setProject('647a6e73beaa05950edd'); // Your project ID
-
 export const account = new Account(client);
