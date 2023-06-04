a1fb7b23642c556fb1b44601093d0b66d3035bfb
geek-nath <nath.codes@gmail.com>
2023-06-04 12:57:17 +0100

    changed variable name

--- src/appwriteConfig.js
+++ src/appwriteConfig.js
@@ -4,4 +4,4 @@ const client = new Client()
     .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
     .setProject('647a6e73beaa05950edd'); // Your project ID
 
-export const registerAccount = new Account(client);
+export const account = new Account(client);
