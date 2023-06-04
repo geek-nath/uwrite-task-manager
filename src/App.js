d8d873bf725441fddc43b986ecaf95206a2dfb11
geek-nath <nath.codes@gmail.com>
2023-06-04 16:24:56 +0100

    imported signup page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/home'
 import SignupPage from './pages/signup'
+import SigninPage from './pages/signin'
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <Routes>
           <Route element={ <HomePage /> } path='/' />
           <Route element={ <SignupPage /> } path='/signup' />
+          <Route element={ <SigninPage /> } path='/signin' />
         </Routes>
       </Router>
     </>
