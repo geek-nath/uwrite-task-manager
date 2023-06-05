6205ace8c3f078138562cce8ba580d848393be3a
geek-nath <nath.codes@gmail.com>
2023-06-05 12:13:40 +0100

    fixed bug

--- src/components/userAvatar.js
+++ src/components/userAvatar.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { account } from '../appwriteConfig';
 import Avatar from 'react-avatar';
-const UserAvatarUI = () => {
+const UserAvatarUI = ({ showNameAndEmail }) => {
   const [ userDetails, setUserDetails ] = useState();
   useEffect(() => {
     const getUserData = account.get();
@@ -15,7 +15,13 @@ const UserAvatarUI = () => {
   }, []);
   return (
     <>
-      <Avatar name={userDetails.name} round={true} size='35'/>
+      { typeof userDetails === 'object' ? <div className='flex gap-2 items-center'>
+        <Avatar name={userDetails.name} round={true} size='35'/>
+        { showNameAndEmail ? <div className='text-left hidden xl:block lg:block'>
+          <h5 className='text-base text-gray-900'>{userDetails.name}</h5>
+          <h6 className='text-sm text-gray-400'>{userDetails.email}</h6>
+        </div> : null }
+      </div> : <img className="h-8 w-8 rounded-full"  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="avatar" /> }
     </>
   )
 }
